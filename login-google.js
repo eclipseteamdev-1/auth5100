3f17ee0839849bc419a6acdc92a0eb56b1474b94
eclipseteamdev-1 <eclipseteamdev1@gmail.com>
2025-07-27 16:25:01 +0700

    Add files via upload

---
+++ login-google.js
@@ -0,0 +1,37 @@
+const firebaseConfig = {
+            apiKey: "AIzaSyCBeFJtPKEMURY-iUDUR4I6gWKjmlTk_3E",
+            authDomain: "localhost",     
+            databaseURL: "https://authdramaarena.firebaseio.com",
+            projectId: "authdramaarena",                      
+            storageBucket: "authdramaarena.appspot.com",      
+            messagingSenderId: "348583435302",             
+            appId: "1:348583435302:web:someUniqueWebId",    
+            measurementId: "G-DGF0CP099H"                  
+        };
+        const app = firebase.initializeApp(firebaseConfig);
+        const auth = firebase.auth();
+        const provider = new firebase.auth.GoogleAuthProvider();
+        document.addEventListener("DOMContentLoaded", function(){
+            
+            document.getElementById("tombol").addEventListener('click', () => {
+            auth.signInWithPopup(provider)
+                .then((result) => {
+                    updateUI(result.user);
+                })
+                .catch((error) => {
+                    console.error('Error signing in with Google:', error);
+                    alert('Error signing in with Google. Please try again.');
+                });
+            });
+            signOutBtn.addEventListener('click', () => {
+                auth.signOut()
+                    .then(() => {
+                        authContainer.classList.remove('hidden');
+                        userInfo.classList.add('hidden');
+                    })
+                    .catch((error) => {
+                       console.error('Error signing out:', error);
+                 });
+        });
+        })
+        
